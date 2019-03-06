238f98250e0c0d49e77933490180b048f9971b43
guillermorosiles3 <43192378+guillermorosiles3@users.noreply.github.com>
2019-03-06 15:34:50 -0600

    Add files via upload
    Fixed

---
+++ Game/BowlingGame/BowlingGame/OpenFrame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class OpenFrame
+    {
+        public int FirstShot { get; set; }
+        public int SecondShot { get; set; }
+    }
+}
